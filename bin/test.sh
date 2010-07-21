13e917607a2b4278c77efacccc518264ce02e3a4
horace.smouthin <horace.smouthin@gmail.com>
2010-07-21 17:53:39 +0000

    test.sh finds the config file alone

--- bin/test.sh
+++ bin/test.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-cd $(dirname $0)
-cd ..
 
 CLASSPATH=$CLASSPATH:$(pwd)/classes
 CLASSPATH=$CLASSPATH:$(pwd)/lib/lwjgl/jar/lwjgl.jar
@@ -9,7 +7,19 @@ CLASSPATH=$CLASSPATH:$(pwd)/lib/lwjgl/jar/lwjgl_util.jar
 CLASSPATH=$CLASSPATH:$(pwd)/lib/lwjgl/jar/jinput.jar
 CLASSPATH=$CLASSPATH:$(pwd)/lib/slick/slick.jar
 
+cd $(dirname $0)
+cd ..
+
+
+config="config/bifstk.conf"
+
+if [ $? -gt 1 ] ; then
+	if [ -f "$1" ] ; then
+		config=$1
+	fi
+fi
+
 java -cp $CLASSPATH \
 	-Djava.library.path=$(pwd)/lib/lwjgl/native/linux/ \
-	bifstk.Test $@
+	bifstk.Test $config
 

54c2fd6efc44f9883d888fd73389fdbf2f2a8e94
horace.smouthin <horace.smouthin@gmail.com>
2010-07-22 16:36:36 +0000

    removed useless Window

--- src/bifstk/type/Frame.java
+++ src/bifstk/type/Frame.java
@@ -1,6 +1,8 @@
 package bifstk.type;
 
-public abstract class Frame implements Drawable {
+import org.lwjgl.opengl.GL11;
+
+public class Frame implements Drawable {
 
 	private Rectangle bounds = null;
 
@@ -18,6 +20,40 @@ public abstract class Frame implements Drawable {
 	public Frame(int x, int y, int w, int h) {
 		this.bounds = new Rectangle(w, h);
 		this.pos = new Point(x, y);
+	}
+
+	@Override
+	public void render() {
+		int x, y, w, h;
+
+		x = this.getX();
+		y = this.getY();
+		w = this.getWidth();
+		h = this.getHeight();
+
+		float alpha = 1.0f;
+		if (this.isDragged()) {
+			alpha = 0.5f;
+		}
+		if (this.isFocused()) {
+			GL11.glColor4f(0.7f, 0.7f, 0.7f, alpha);
+		} else {
+			GL11.glColor4f(0.5f, 0.5f, 0.5f, alpha);
+		}
+		GL11.glBegin(GL11.GL_QUADS);
+		GL11.glVertex2i(x, y);
+		GL11.glVertex2i(x + w, y);
+		GL11.glVertex2i(x + w, y + h);
+		GL11.glVertex2i(x, y + h);
+		GL11.glEnd();
+
+		GL11.glColor3f(0.0f, 0.0f, 0.0f);
+		GL11.glBegin(GL11.GL_LINE_LOOP);
+		GL11.glVertex2i(x, y);
+		GL11.glVertex2i(x + w, y);
+		GL11.glVertex2i(x + w, y + h);
+		GL11.glVertex2i(x, y + h);
+		GL11.glEnd();
 	}
 
 	public int getX() {

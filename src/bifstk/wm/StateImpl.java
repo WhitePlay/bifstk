a4e293a920fed9c369a12ced232c14f7cde94088
horace.smouthin <horace.smouthin@5179dca0-5831-4f2f-5489-3ff441c1facc>
2010-07-25 14:25:20 +0000

    reorganized source folder

---
+++ src/bifstk/wm/StateImpl.java
@@ -0,0 +1,83 @@
+package bifstk.wm;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+
+/**
+ * Window Manager's internal state
+ * <p>
+ * Maintains the window manager's Frames in two separate collections with
+ * different visibilities
+ * 
+ */
+public class StateImpl implements State {
+
+	/** frames as Frame for internal use */
+	private Deque<Frame> _frames = null;
+	/** frames as Drawable for API exposition */
+	private Deque<Drawable> frames = null;
+
+	public StateImpl() {
+		this.frames = new ArrayDeque<Drawable>();
+		this._frames = new ArrayDeque<Frame>();
+	}
+
+	@Override
+	public Deque<Drawable> getFrames() {
+		return this.frames;
+	}
+
+	/**
+	 * Creates a new Frame in the WM
+	 * 
+	 * @param x
+	 * @param y
+	 */
+	public void addFrame(int x, int y) {
+		Frame f = new Frame(x, y);
+
+		if (this._frames.size() > 0) {
+			this._frames.getFirst().setFocused(false);
+		}
+		// frame is added on top of the stack: focused
+		this.frames.addFirst(f);
+		this._frames.addFirst(f);
+
+		f.setFocused(true);
+	}
+
+	/**
+	 * Give the focus to a frame
+	 * 
+	 * @param tofocus frame to focus
+	 */
+	public void focusFrame(Frame tofocus) {
+		if (this._frames.size() > 0) {
+			this._frames.getFirst().setFocused(false);
+		}
+		if (tofocus != null) {
+			tofocus.setFocused(true);
+			this._frames.remove(tofocus);
+			this._frames.addFirst(tofocus);
+			this.frames.remove(tofocus);
+			this.frames.addFirst(tofocus);
+		}
+	}
+
+	/**
+	 * Finds a frame in the WM
+	 * 
+	 * @param x abscissa
+	 * @param y ordinate
+	 * @return the frame under the provided coordinates, or null
+	 */
+	public Frame findFrame(int x, int y) {
+		for (Frame frame : _frames) {
+			if (frame.contains(x, y)) {
+				return frame;
+			}
+		}
+		return null;
+	}
+}

7e596f856dd0b4cbd4b56844fcccc541b875aea3
horace.smouthin <horace.smouthin@gmail.com>
2011-04-25 17:31:02 +0000

    prettier frame controls

--- src/test/Test.java
+++ src/test/Test.java
@@ -79,21 +79,9 @@ public class Test implements Handler, Root {
 		int imgY = (dh - h) / 2;
 		Util.raster().fillQuad(imgX, imgY, bgImg, 1.0f);
 
-		/* display the atlas 
-		GL11.glEnable(GL11.GL_TEXTURE_2D);
-		GL11.glColor3f(1.0f, 1.0f, 1.0f);
-		GL11.glBindTexture(GL11.GL_TEXTURE_2D, Atlas.getInstance().getTexId());
-		GL11.glBegin(GL11.GL_QUADS);
-		GL11.glTexCoord2f(0.0f, 0.0f);
-		GL11.glVertex2i(0, 0);
-		GL11.glTexCoord2f(1.0f, 0.0f);
-		GL11.glVertex2i(512, 0);
-		GL11.glTexCoord2f(1.0f, 1.0f);
-		GL11.glVertex2i(512, 512);
-		GL11.glTexCoord2f(0.0f, 1.0f);
-		GL11.glVertex2i(0, 512);
-		GL11.glEnd();
-		*/
+		/* display the atlas */
+		// Util.raster().fillQuad(0, 0, 512, 512, new Image(0, 0, 512, 512),
+		//		Color.WHITE, 1.0f, Rotation.ROTATE_0);
 	}
 
 	@Override
